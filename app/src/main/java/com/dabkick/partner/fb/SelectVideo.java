0844693994dc2bad52284cae2641ce34e910696f
shwetha@dabkick.com <welcometodabkick>
2016-04-11 12:15:48 +0530

    Shwetha_11Apr16:
    1. Video play non LS

--- app/src/main/java/com/dabkick/partner/fb/SelectVideo.java
+++ app/src/main/java/com/dabkick/partner/fb/SelectVideo.java
@@ -11,113 +11,90 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.dabkick.sdk.Dabkick;
+import com.dabkick.sdk.Global.GlobalHandler;
+import com.dabkick.sdk.Global.HorizontalListView;
+import com.dabkick.sdk.Global.PreferenceHandler;
+import com.dabkick.sdk.Global.VideoManager;
+import com.dabkick.sdk.Livesession.VideoHorizontalAdapter;
+import com.dabkick.sdk.Livesession.YouTubeVideoDetail;
+import com.dabkick.sdk.Video.PlayDabKickVideoActivity;
 
 import java.util.ArrayList;
 
 public class SelectVideo extends AppCompatActivity {
 
-    /*//your own listview
-    public com.dabkick.sdk.Horizontal.HorizontalListView hListView;
+    //your own listview
+    public HorizontalListView hListView;
     //your own adapter
     public VideoHorizontalAdapter mVideoHorizontalAdapter;
     //Dabkick agent init to make use of lib
-    DabKickVideoManagerAgent dabKickVideoManagerAgent = DabKickVideoManagerAgent.getInstance();
+    VideoManager videoManager = VideoManager.getInstance();
     //local array list to get the results
     ArrayList VideosList;
-    private Button goToLs;
 
     //Dabkickvideodetail
-    DabKickVideoDetail mDabKickVideoDetailSingleItem;
+    YouTubeVideoDetail mYoutubeVideoDetailSingleItem;
 
     TextView statusMsg,userInfo;
     Button watchWithFriends;
 
-    ProgressBar mProgressBar;*/
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_video);
 
-        Button watchWithFriends = (Button)findViewById(R.id.wwf) ;
-
-                watchWithFriends.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Dabkick.watchWithFriends(SelectVideo.this, "uu9XbrApmXE");
-
-            }
-        });
-
-       /* init();
+        init();
 
         statusMsg.setMovementMethod(new ScrollingMovementMethod());
-        DabKick_Agent.displayStatusMessages(statusMsg);
 
-        userInfo.setMovementMethod(new ScrollingMovementMethod());
-        DabKick_Agent.displayUserInfo(userInfo);
+        String loginInfo = "Email:"+ PreferenceHandler.getEmail()+ "\nPhone number:"+PreferenceHandler.getPhoneNum()
+                +"\nUnique ID:"+PreferenceHandler.getUniqueID();
+        statusMsg.setText(loginInfo);
 
-        goToLs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DabKick_Agent.goToLiveSession(SelectVideo.this);
-            }
-        });
-
-//        watchWithFriends.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                DabKickRegisterAgent.watchWithFriends(SelectVideo.this);
-//
-//            }
-//        });
         mProgressBar.setVisibility(View.VISIBLE);
 
         hListView = (HorizontalListView) findViewById(R.id.testvideosListView);
-            //make a call to this agent to get the search results and pass the results to your adapter to show it in list view
-            dabKickVideoManagerAgent.setOnSearchFinishedLoading(new DabKickVideoManagerAgent.OnSearchFinishedLoadingListener() {
-                @Override
-                public ArrayList onSearchFinishedLoading(boolean success) {
-                    DabKickGlobalData.runOnUIThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            mProgressBar.setVisibility(View.GONE);
-                            //call this method with the search term to get the results
-                            VideosList = dabKickVideoManagerAgent.getSearchResultByTerm(dabKickVideoManagerAgent.TERM_FB);
-                            mVideoHorizontalAdapter = new VideoHorizontalAdapter(SelectVideo.this, R.layout.video_view_item, VideosList, false);
-                            hListView.setAdapter(mVideoHorizontalAdapter);
-                            mVideoHorizontalAdapter.notifyDataSetChanged();
-                        }
-                    });
-                    return null;
-                }
-            });
-            //Condition check to load the searched videos if not throw alert(logic done in DabKickVideoManagerAgent)
-            dabKickVideoManagerAgent.searchVideo(dabKickVideoManagerAgent.TERM_FB);
+
+        //make a call to this agent to get the search results and pass the results to your adapter to show it in list view
+        videoManager.setOnSearchFinishedLoading(new VideoManager.OnSearchFinishedLoadingListener() {
+            @Override
+            public void onSearchFinishedLoading(boolean b) {
+                GlobalHandler.runOnUIThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mProgressBar.setVisibility(View.GONE);
+                        //call this method with the search term to get the results
+                        VideosList = videoManager.getSearchResultByTerm("telcom");
+                        mVideoHorizontalAdapter = new VideoHorizontalAdapter(SelectVideo.this, R.layout.video_view_item, VideosList, false);
+                        hListView.setAdapter(mVideoHorizontalAdapter);
+                        mVideoHorizontalAdapter.notifyDataSetChanged();
+
+                    }
+                });
+            }
+        });
+        //Condition check to load the searched videos if not throw alert(logic done in DabKickVideoManagerAgent)
+        videoManager.searchVideo("telcom");
 
         hListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Call this method to get the selected video
-                mDabKickVideoDetailSingleItem = (DabKickVideoDetail)mVideoHorizontalAdapter.getItem(position);
-                //PlayDabKickVideoActivity is an dabkick library class, use this to get a lib built-in player.
+                mYoutubeVideoDetailSingleItem = (YouTubeVideoDetail)mVideoHorizontalAdapter.getItem(position);
                 Intent intent = new Intent(SelectVideo.this, PlayDabKickVideoActivity.class);
-                intent.putExtra(PlayDabKickVideoActivity.EXTRA_VIDEO_ID, mDabKickVideoDetailSingleItem.videoID);
+                intent.putExtra(PlayDabKickVideoActivity.EXTRA_VIDEO_ID, mYoutubeVideoDetailSingleItem.videoID);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
-        });*/
+        });
     }
 
     void init(){
 
-        /*statusMsg = (TextView)findViewById(R.id.statusMsg);
-        userInfo = (TextView)findViewById(R.id.userInfo) ;
-//        watchWithFriends = (Button)findViewById(R.id.wwf) ;
+        statusMsg = (TextView)findViewById(R.id.statusMsg);
         mProgressBar = (ProgressBar)findViewById(R.id.progress_bar);
-        goToLs = (Button) findViewById(R.id.go_to_ls_btn);
-*/
+
     }
 }
